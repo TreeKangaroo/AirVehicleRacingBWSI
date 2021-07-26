b70c36cd7620d06d36c98c768fa5213bae9ec484
keiji-imai <keiji.d.imai@gmail.com>
2021-07-26 14:25:55 -0400

    Hoop detection with hoop class

--- General/Hoop.py
+++ General/Hoop.py
@@ -18,22 +18,24 @@ class Hoop:
         import numpy as np
         
         hsv=cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
+        self.mask = cv2.inRange(hsv, lower_thresh, upper_thresh)
         kernel = np.ones((5,5),np.uint8)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        res = cv2.bitwise_and(img, img, mask=mask)
+        self.mask = cv2.morphologyEx(self.mask, cv2.MORPH_OPEN, kernel)
+        self.mask = cv2.morphologyEx(self.mask, cv2.MORPH_CLOSE, kernel)
+        self.res = cv2.bitwise_and(img, img, mask=self.mask)
     
-        grayscale = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY);
-        grayscale = cv2.dilate(grayscale,kernel,iterations = 1)
         
-        contours, hierarchy = cv2.findContours(grayscale, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        self.contours, hierarchy = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        self.contour = max(contours, key = cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(self.contour)
-        self.rectangle=[x,y,x+w,y+h]
-        self.ellipse = cv2.fitEllipse(self.contour)
-        self.center = (int(self.ellipse[0][0]), int(self.ellipse[0][1]))
+        if len(self.contours)>0:
+            self.contour = max(self.contours, key = cv2.contourArea)
+            x,y,w,h = cv2.boundingRect(self.contour)
+            self.rect=(x,y,w,h)
+            if self.contour.shape[0] > 5:
+                self.ellipse = cv2.fitEllipse(self.contour)
+                self.center = (int(self.ellipse[0][0]), int(self.ellipse[0][1]))
+            else:
+                self.center = (int(x+w//2), int(y+h//2))
         
 
 
--- Keiji/keiji_hoop.py
+++ Keiji/keiji_hoop.py
@@ -1,16 +1,18 @@
 from djitellopy import Tello
 import cv2 as cv
 import numpy as np
-import queue
-import threading 
+import threading
+import time
+import sys
+sys.path.append('../')
+from General.Hoop import Hoop
 
-q = queue.Queue()
 tello = Tello()
 tello.connect()
 print(tello.get_battery())
 tello.streamon()
 inp = '0'
-debug = False
+debug = True
 if not debug: 
     tello.takeoff()
 
@@ -26,7 +28,7 @@ def PID(Kp, Ki, Kd, MV_bar=0):
         I = I + Ki*e*(t - t_prev)
         D = Kd*(e - e_prev)/(t - t_prev)
         MV = MV_bar + P + I + D
-        #print(f'MV:{int(MV)} P:{int(P)} I:{int(I)} D:{int(D)} e:{e} e_prev:{e_prev}')
+        print(f'MV:{int(MV)} P:{int(P)} I:{int(I)} D:{int(D)} e:{e} e_prev:{e_prev}')
 
         e_prev = e
         t_prev = t
@@ -70,36 +72,47 @@ x = 0
 def main():
     global inp
     while inp != 'q':
+        t = time.time()
         frame = tello.get_frame_read().frame
-        hsv = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
-        mask = cv.inRange(hsv, lower_blue, upper_blue)
-        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-        mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
-        mask = cv.erode(mask, kernel, iterations=1)
-        res = cv.bitwise_and(frame, frame, mask=mask)
+        res = frame
+
+        hoop = Hoop(frame, lower_blue, upper_blue)
+
+        # hsv = cv.cvtColor(frame, cv.COLOR_RGB2HSV)
+        # mask = cv.inRange(hsv, lower_blue, upper_blue)
+        # mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+        # mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+        # mask = cv.erode(mask, kernel, iterations=1)
+        # res = cv.bitwise_and(frame, frame, mask=mask)
 
         frameCenter = (frame.shape[1] // 2, frame.shape[0] //2)
-        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        if len(contours) > 0:
-            c = max(contours, key = cv.contourArea)
-            x,y,w,h = cv.boundingRect(c)
-            ellipse = cv.fitEllipse(c)
-            hoopCenter = (int(ellipse[0][0]), int(ellipse[0][1]))
+        if len(hoop.contours) > 0:
+            c = hoop.contour
 
-            cv.drawContours(res, contours, -1, (255, 0, 0), 2)
-            cv.rectangle(res,(x,y),(x+w,y+h),(0,255,0),5)
-            cv.circle(res, hoopCenter, 5, (255, 0, 0), -1)
-            cv.ellipse(res, ellipse, (0, 255, 0), 5)
+            # x,y,w,h = cv.boundingRect(c)
+            if c.shape[0] > 5:
+                ellipse = hoop.ellipse
+                cv.ellipse(res, ellipse, (0, 255, 0), 5)
+            x,y,w,h = hoop.rect
+            res = hoop.res
+            cv.drawContours(res, hoop.contours, -1, (255, 0, 0), 2) #Green fitted ellipse
+            cv.rectangle(res,(x,y),(x+w,y+h),(0,255,0),5) #Green bounding rectangle
+            cv.circle(res, hoop.center, 5, (255, 0, 0), -1) #Red circle in center of hoop
 
+            z = int(z_controller.send((t, hoop.center[1], frameCenter[1])))
+        
+        if not debug: tello.send_rc_control(x, y, z, yaw)
 
-
+        cv.circle(res, frameCenter, 5, (0, 0, 255), -1) #Blue circle in center of screen
         cv.imshow("camera", res)
-        key = cv.waitKey(5)
+        key = cv.waitKey(1000)
         if key & 0xFF == ord('q'):
             print("DONE")
             tello.end()
             break
+    print('ENDING')
     tello.end()
 
 if __name__=='__main__':
