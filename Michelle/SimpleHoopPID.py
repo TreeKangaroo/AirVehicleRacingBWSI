2f777be4b7e32e0d1b2a6295679ae06f22899f6c
TreeKangaroo <michellewang.joy@gmail.com>
2021-07-29 11:24:41 -0500

    Added a disance PID controller

--- Michelle/SimpleHoopPID.py
+++ Michelle/SimpleHoopPID.py
@@ -13,6 +13,8 @@ tello = Tello()
 tello.connect()
 print(tello.get_battery())
 tello.streamon()
+frame_generator=tello.get_frame_read()
+frame=frame_generator.frame
 inp = '0'
 debug = False
 if not debug: 
@@ -37,16 +39,16 @@ def PID(Kp, Ki, Kd, MV_bar=0):
         t_prev = t
 
 #PID Values
-yaw_controller = PID(0.36, 0, 0.1, MV_bar=5)
+yaw_controller = PID(0.3, -0.000000000000002, 0.1, MV_bar=5)
 yaw_controller.send(None)
 
 z_controller = PID(0.2, 0, 0.05)
 z_controller.send(None)
 
-x_controller = PID(0.1, 0, 0.03)
+x_controller = PID(0.07,-0.000000000005, 0.05)
 x_controller.send(None)
 
-y_controller = PID(0.1, 0, 0)
+y_controller = PID(0.5, 0, 0.03)
 y_controller.send(None)
 
 #Pool Noodle Thresholds
@@ -74,15 +76,20 @@ def main():
     z = 0
     y = 0
     x = 0
+    
     last_yaw=0
+    target_distance = 150
+    kyawx=3
+    
     state = 0
     count = 0
-    target = 40
-    targetOffset = 150
+    target = 30
+    targetOffset = 140
+    font = cv.FONT_HERSHEY_SIMPLEX
     print("State 0: Line up with noodle")
     while inp != 'q':
         t = time.time()
-        frame = tello.get_frame_read().frame
+        frame = frame_generator.frame
         frameCenter = (frame.shape[1] // 2, frame.shape[0] //2 - targetOffset)
         res = frame
 
@@ -94,7 +101,7 @@ def main():
             hoop = Hoop(frame, lower_orange, upper_orange)
 
         if state == 0: #Line up with noodle
-            y = 0
+            y = 20
             if hoop.seenHoop:
 
                 res = hoop.res
@@ -109,24 +116,37 @@ def main():
                 cv.line(res, hoop.center, tuple(hoop.imgpts[1,0]), (0,255,0), 5)
                 cv.line(res, hoop.center, tuple(hoop.imgpts[2,0]),(0,0,255), 5)
                 
-               # pitch_ratio=abs(math.tan(hoop.euler[0]))
+                    
+                pitch_ratio=abs(math.tan(hoop.euler[0]))
                 yaw_angle=abs(int(hoop.euler[1]))
-                #distance=(np.sum(hoop.tvecs**2))**0.5
+                #yaw_comp_x=kyawx*math.sin(yaw_angle)
+                distance=(np.sum(hoop.tvecs**2))**0.5
                 
                 #z_pitchcomp=y*pitch_ratio
                 if yaw_angle>last_yaw:
                     yaw_angle=-1*yaw_angle
-                    last_yaw=yaw_angle
+                    last_yaw=abs(yaw_angle)
                     
                 z = int(z_controller.send((t, hoop.center[1], frameCenter[1])))
                 x = -1 * int(x_controller.send((t, hoop.center[0], frameCenter[0])))
+                y = -1*int(y_controller.send((t, distance, target_distance)))
                 yaw=int(yaw_controller.send((t, yaw_angle, 0)))
+                
+                if (rectY < 80): textOrigin = (rectX, rectY + rectH + 40)
+                else: textOrigin = (rectX, rectY-10)
+                hoopText = 'Pitch:{:4d} Yaw:{:3d} Dist:{}'.format(int(hoop.euler[0]), yaw_angle, distance)
+                cv.putText(res, hoopText, textOrigin, font, 0.75, (255, 255, 255), 2, cv.LINE_AA)
+
+                bottomText = 'State:{} X:{:3d} Y:{:3d} Z:{:3d} Yaw: {:3d} PitchRatio:{:.3f}'.format(state, x, y, z, yaw, pitch_ratio)
+                print(bottomText)
+                cv.putText(res, bottomText, (0,40), font, 1, (255, 255, 255), 2, cv.LINE_AA)
 
                 z_error = frameCenter[1] - hoop.center[1]
                 x_error = frameCenter[0] - hoop.center[0]
+                distance_error = abs(distance - target_distance)
                 error_mag = math.sqrt(z_error**2 + x_error**2)
                 #print(z_error, x_error, error_mag)
-                if (error_mag < target and x < 20 and z < 20 and yaw<15): 
+                if (error_mag < target and x < 20 and z < 20 and yaw<15 and distance_error<20): 
                     state = 1 #Comment out this line to debug with a single hoop
                     print("State 1: Approach noodle")
         elif state == 1:
